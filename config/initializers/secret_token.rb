7fe14e865456e56be46aaa3cfd9b0016a6654d15
Ross Hepburn <hepburnjrl@gmail.com>
2014-04-10 12:14:58 +0100

    Ability to add restaurant created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YelpClone::Application.config.secret_key_base = '03df5b7140dd2a0e64ec970ea81ee3bc6d90103b563ff4ce130ce505802b87c5e72ac8eeca7f425bfc090898d8c0a2a59c0a3117cfe28c5fabdb2fa8c8ff6121'
